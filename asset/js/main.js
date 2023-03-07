6c5674b23cfe60c9c4f3598ee61f79a4dbeb4373
CatalanoLorenzo <lorenzocatalano1995@outlook.it>
2023-03-07 14:45:08 +0100

    add main.js complit

---
+++ asset/js/main.js
@@ -0,0 +1,35 @@
+/* 
+quest:
+Decrizione passaggi:
+Chiedi all'utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+Infine scrivi sulla pagina il risultato usando questo formato: nomecognomecolorepreferito23
+ */
+//debug connect main.js on index.html
+alert("main.js connect")
+
+
+
+//request user'name
+let userName;
+userName = prompt("Qual'è il tuo Nome");
+console.log(userName);
+
+//request user'surname
+let userSurname;
+userSurname = prompt("Qual'è il tuo Cognome");
+console.log(userSurname);
+
+//request user'favorit color
+let usercolor;
+usercolor = prompt("Qual'è il tuo Colore preferito");
+console.log(usercolor);
+
+//whrite on page the result 
+let result
+result = userName + userSurname + usercolor + 23;
+console.log(result);
+let element = document.getElementById("result_display");
+console.log (element);
+element.innerHTML = result;
